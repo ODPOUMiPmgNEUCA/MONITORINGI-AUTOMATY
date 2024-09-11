7730929cbea84fbab6d9d95a2a26567046be23fa
ODPOUMiPmgNEUCA <mgroblica@neuca.pl>
2024-09-11 16:32:06 +0200

    Update monitoringi_automaty.py

--- monitoringi_automaty.py
+++ monitoringi_automaty.py
@@ -401,13 +401,6 @@ if sekcja == 'Paramig Fast Junior 250MG':
 
 
 
-
-
-
-
-
-    
-    
     #plik z poprzedniego monitoringu
     poprzedni = st.file_uploader(
         label = "Wrzuć plik z poprzedniego monitoringu"
@@ -419,7 +412,7 @@ if sekcja == 'Paramig Fast Junior 250MG':
 
     poprzedni = poprzedni.rename(columns={'max_percent': 'old_percent'})
     # Wykonanie left join, dodanie 'old_percent' do pliku 'ostatecznie'
-    result = ostatecznie.merge(poprzedni[['Kod klienta', 'old_percent']], on='Kod klienta', how='left')
+    result = max_rabaty.merge(poprzedni[['Kod klienta', 'old_percent']], on='Kod klienta', how='left')
     result['old_percent'] = result['old_percent'].fillna(0)
     result['Czy dodać'] = result.apply(lambda row: 'DODAJ' if row['max_percent'] > row['old_percent'] else '', axis=1)
     st.write('Kliknij aby pobrać plik z kodami, które kody należy dodać')
