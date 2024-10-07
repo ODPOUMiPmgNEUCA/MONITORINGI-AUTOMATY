cfd791ffc1b82c8231e3f6e2af6ead7740b08dc8
ODPOUMiPmgNEUCA <mgroblica@neuca.pl>
2024-10-07 10:38:35 +0200

    Update monitoringi_automaty.py

--- monitoringi_automaty.py
+++ monitoringi_automaty.py
@@ -455,12 +455,10 @@ if sekcja == 'Brazoflamin':
 
 
     wynik_df = pd.merge(powiazanie, ims, left_on='KLIENT', right_on='Klient', how='left')
-    wynik_df
 
 
     # Wybór potrzebnych kolumn: 'APD_kod_SAP_apteki' i 'max_percent'
     wynik_df = wynik_df[['KLIENT','APD_kod_SAP_apteki', 'max_percent']]
-    wynik_df
     
     
     #to są kody SAP
