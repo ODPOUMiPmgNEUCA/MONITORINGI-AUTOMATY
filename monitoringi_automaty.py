8c93a1eac3aa806c1f0e9229a049bd105d265e6a
ODPOUMiPmgNEUCA <mgroblica@neuca.pl>
2025-02-24 11:57:10 +0100

    Update monitoringi_automaty.py

--- monitoringi_automaty.py
+++ monitoringi_automaty.py
@@ -248,7 +248,7 @@ if sekcja == 'Musy':
 
         # Sprawdzamy, które arkusze są dostępne i wczytujemy odpowiednie dane
         if 'Gratisy, rabat' in xls.sheet_names:
-            Gratisy = pd.read_excel(df, sheet_name='Rabat', skiprows=15, usecols=[1, 2, 8])
+            Gratisy = pd.read_excel(df, sheet_name='Rabat', skiprows=15, usecols=[1, 2, 9])
             st.write("Dane z arkusza Gratisy, rabat:")
             st.write(Gratisy.head())
 
